d54e7b7b82e0a2847af079239d1a7bf2f2dcda6a
Edward Chen <chenward.T@gmail.com>
2016-02-08 19:48:50 -0500

    Change PollOption's data_by_poll scope to return hash instead of array.

--- app/models/poll_option.rb
+++ app/models/poll_option.rb
@@ -2,5 +2,6 @@ class PollOption < ApplicationRecord
   # TODO: Ensure uniqueness of keys in data_by_poll.
   belongs_to :poll
 
-  scope :data_by_poll, -> (id) { where(poll_id: id).pluck(:text, :count) }
+  scope :data_by_poll, -> (id) { where(poll_id: id).pluck(:text, :count)
+                                 .map { |plucked| Hash[[:text, :count].zip(plucked)] } }
 end
